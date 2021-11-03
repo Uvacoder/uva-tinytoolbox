ce49e90f652513faa0d9d070d47e508c5a6a07f1
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-11-03 08:53:53 -0400

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -24,8 +24,8 @@ module.exports = {
         'https://avatars0.githubusercontent.com/u/10353856?s=460&u=88394dfd67727327c1f7670a1764dc38a8a24831&v=4',
       name: 'Vitaly Rtishchev',
       tg: 'rtivital',
-      github: 'rtivital',
-      website: 'https://github.com/rtivital',
+      github: 'uvacoder',
+      website: 'https://github.com/uvacoder',
     },
 
     contributors: [
