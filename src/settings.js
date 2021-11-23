8a3abd7d4a3aee47b7da4819963b0364c8a7212a
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-11-22 23:46:31 -0500

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  repository: 'https://github.com/rtivital/omatsuri',
+  repository: 'https://github.com/uvacoder/tools-resources',
   bugs: 'https://github.com/rtivital/omatsuri/issues/new',
 
   meta: [
@@ -22,8 +22,8 @@ module.exports = {
     author: {
       avatar:
         'https://avatars0.githubusercontent.com/u/10353856?s=460&u=88394dfd67727327c1f7670a1764dc38a8a24831&v=4',
-      name: 'Vitaly Rtishchev',
-      tg: 'rtivital',
+      name: 'Bill Lumberg',
+      tg: 'uvacoder',
       github: 'uvacoder',
       website: 'https://github.com/uvacoder',
     },
